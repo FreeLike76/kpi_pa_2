40202f742f210b867502c532182fb35c630962fd
Dmytro Geleshko <dmytro.geleshko01@gmail.com>
2020-09-29 13:30:19 +0300

    Greedy algorithm for ABC startup is done

--- kpi_pa_2.cpp
+++ kpi_pa_2.cpp
@@ -9,10 +9,18 @@ using namespace std;
 
 #define defPath "matrix.txt"
 
+void greedColor(Graph& graph);
+
 int main()
 {
     Graph graph(defPath);
-
+	greedColor(graph);
+	for (int i = 0; i < graph.getSize(); i++)
+	{
+		cout << i << "\t" << graph.pow[i] << "\t" << graph.color[i] << endl;
+	}
+	auto colornum = max_element(graph.color.begin(), graph.color.end());
+	cout << "Total: " << *colornum + 1<< endl;
 }
 
 void greedColor(Graph& graph)
@@ -29,16 +37,36 @@ void greedColor(Graph& graph)
 			return vert1.second > vert2.second;
 		});
 
-	//if noColor==color.end() all vert hava a color;
-	vector<int>::iterator noColor;
-	do
+	int color = 0;
+	bool paint = true;
+
+	for (int i = 0; i < graph.getSize(); i++)
 	{
-		for (int i = 0; i < graph.getSize(); i++)
+		if (graph.color[vertOrder[i].first] == -1)
 		{
-
+			graph.color[vertOrder[i].first] = color;
+			for (int j = 0; j < graph.getSize(); j++)
+			{
+				if (graph.adj[vertOrder[i].first][j] == 0&&graph.color[j]==-1)
+				{
+					for (int k = 0; k < graph.getSize(); k++)
+					{
+						if (graph.adj[j][k] == 1 && graph.color[k] == color)
+						{
+							paint = false;
+							break;
+						}
+					}
+					if (paint)
+						graph.color[j] = color;
+					else
+					{
+						paint = true;
+					}
+				}
+			}
+			color++;
 		}
-		noColor = find(graph.color.begin(), graph.color.end(), -1);
-	} while (noColor != graph.color.end());
-	
-
+	}
+	vertOrder.clear();
 }
